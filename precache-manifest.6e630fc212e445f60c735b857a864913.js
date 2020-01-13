11ea17bd83ee6a6fe0965c121738d22383705117
abduljilani <abdul.jilani@bold.com>
2020-01-13 14:52:04 +0530

    Updates

---
+++ precache-manifest.6e630fc212e445f60c735b857a864913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dcaadf9dc16c1fea3b93110d858df11",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "74fb7e4189858f952f32",
+    "url": "/monsters-rolodex/static/css/main.c87b2398.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74fb7e4189858f952f32",
+    "url": "/monsters-rolodex/static/js/main.03080d01.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
